7aaa10092b80cea1829d8c16f4009a964824f878
minhl337 <39414218+minhl337@users.noreply.github.com>
2020-10-11 23:06:40 -0500

    created contact display components

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,43 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const ContactItem = ({ contact: { name, id, email, phone, type } }) => {
+  return (
+    <div className="card bg-light ">
+      <h3
+        className="text-primary text-left"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        {name}{" "}
+        <span
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {type.charAt(0).toUpperCase() + type.slice(1)}
+        </span>
+      </h3>
+      <ul className="list">
+        {email && (
+          <li>
+            <i className="fas fa-envelope-open"> {email}</i>
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className="fas fa-phone"> {phone}</i>
+          </li>
+        )}
+      </ul>
+      <button className="btn btn-dark btn-sm">Edit</button>
+      <button className="btn btn-danger btn-sm">Delete</button>
+    </div>
+  )
+}
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired,
+}
+
+export default ContactItem
