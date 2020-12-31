37f9b2c704e6d2b1b785f1ea7f5141e6b9846416
minhl337 <imnotminh@gmail.com>
2020-12-30 23:51:43 -0600

    add login and register components

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,52 @@
+import React from "react"
+
+const Login = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  })
+
+  const { name, email, password, password2 } = user
+
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(user)
+  }
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" value={email} onChange={onChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Log In"
+          className="btn btn-primary btn-block"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Login
